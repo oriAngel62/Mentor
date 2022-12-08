609f5ac6961a3c64be00e987e638661a8f20593c
David <davidgurovich580@gmail.com>
2022-12-08 16:54:29 +0200

    beforeInstall-beforeTestingTabs

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import toolbox from "../components/toolbox"
 // export async function getStaticProps() {
 // const allPostsData = getSortedPostsData();
 // return {
@@ -14,5 +15,10 @@ import Date from "../components/date";
 //}
 
 export default function Home({ allPostsData }) {
-    return <Link href="./schedule">david</Link>;
+    return (
+    <div>
+    <Link href="./schedule">david</Link>
+    <toolbox/>
+    </div>
+    );
 }
