208cd06de9bf357b6b2aee0654f99f544e115bd3
Ori <oriangel62@gmail.com>
2023-06-12 15:00:14 +0300

    change cover

--- modules/views/ProductHero.js
+++ modules/views/ProductHero.js
@@ -1,50 +1,60 @@
-import * as React from 'react';
-import Button from '../components/Button';
-import Typography from '../components/Typography';
-import ProductHeroLayout from './ProductHeroLayout';
+import * as React from "react";
+import Button from "../components/Button";
+import Typography from "../components/Typography";
+import ProductHeroLayout from "./ProductHeroLayout";
 
 const backgroundImage =
-  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400';
+    "https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=900&t=st=1686570705~exp=1686571305~hmac=53a539a9a75c469c0e036462f7414110a29add8d178af3619816cb6352ce7b83";
 
 export default function ProductHero() {
-  return (
-    <ProductHeroLayout
-      sxBackground={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundColor: '#7fc7d9', // Average color of the background image.
-        backgroundPosition: 'center',
-      }}
-    >
-      {/* Increase the network loading priority of the background image. */}
-      <img
-        style={{ display: 'none' }}
-        src={backgroundImage}
-        alt="increase priority"
-      />
-      <Typography color="inherit" align="center" variant="h2" marked="center">
-        Upgrade your Schedule
-      </Typography>
-      <Typography
-        color="inherit"
-        align="center"
-        variant="h5"
-        sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
-      >
-        Enjoy secret offers up to -70% off the best luxury hotels every Sunday.
-      </Typography>
-      <Button
-        color="secondary"
-        variant="contained"
-        size="large"
-        component="a"
-        href="/in/sign-up/"
-        sx={{ minWidth: 200 }}
-      >
-        Register
-      </Button>
-      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
-        Discover the experience
-      </Typography>
-    </ProductHeroLayout>
-  );
+    return (
+        <ProductHeroLayout
+            sxBackground={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundColor: "#7fc7d9", // Average color of the background image.
+                backgroundPosition: "center",
+            }}
+        >
+            {/* Increase the network loading priority of the background image. */}
+            <img
+                style={{ display: "none" }}
+                src={backgroundImage}
+                alt="increase priority"
+            />
+            <Typography
+                color="inherit"
+                align="center"
+                variant="h2"
+                marked="center"
+            >
+                Upgrade your Schedule
+            </Typography>
+            <Typography
+                color="inherit"
+                align="center"
+                variant="h5"
+                sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
+            >
+                One click from being one of mentor:
+            </Typography>
+            <Button
+                color="secondary"
+                variant="contained"
+                size="large"
+                component="a"
+                href="/in/sign-up/"
+                sx={{ minWidth: 300 }}
+            >
+                Register
+            </Button>
+            <Typography
+                size="large"
+                variant="body2"
+                color="inherit"
+                sx={{ mt: 2 }}
+            >
+                ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑{" "}
+            </Typography>
+        </ProductHeroLayout>
+    );
 }
