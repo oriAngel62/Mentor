9f9f99cd17e10dab7b8abdc07d4ab719cf861a10
Ori <oriangel62@gmail.com>
2023-06-17 10:30:10 +0300

    cover

--- modules/views/ProductHowItWorks.js
+++ modules/views/ProductHowItWorks.js
@@ -1,127 +1,136 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Button from '../components/Button';
-import Typography from '../components/Typography';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Button from "../components/Button";
+import Typography from "../components/Typography";
 
-import Image from 'next/image';
-import productHowItWorks1 from  "/public/images/productHowItWorks1.svg";
-import productHowItWorks2 from "/public/images/productHowItWorks2.svg";
-import productHowItWorks3 from "/public/images/productHowItWorks3.svg";
-import productCurvyLines from "/public/images/productCurvyLines.png"
+import Image from "next/image";
+import productHowItWorks1 from "/public/images/productHowItWorks1.svg";
+import magic from "/public/images/magic.svg";
+import schedule from "/public/images/schedule.svg";
+import productCurvyLines from "/public/images/productCurvyLines.png";
 
 const item = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  px: 5,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    px: 5,
 };
 
 const number = {
-  fontSize: 24,
-  fontFamily: 'default',
-  color: 'secondary.main',
-  fontWeight: 'medium',
+    fontSize: 24,
+    fontFamily: "default",
+    color: "secondary.main",
+    fontWeight: "medium",
 };
 
 const image = {
-  height: 55,
-  my: 4,
+    height: 100,
+    my: 4,
 };
 
 function ProductHowItWorks() {
-  return (
-    <Box
-      component="section"
-      sx={{ display: 'flex', bgcolor: 'secondary.light', overflow: 'hidden' }}
-    >
-      <Container
-        sx={{
-          mt: 10,
-          mb: 15,
-          position: 'relative',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
+    return (
         <Box
-          component={Image}
-          src={productCurvyLines}
-          alt="curvy lines"
-          sx={{
-            pointerEvents: 'none',
-            position: 'absolute',
-            top: -180,
-            opacity: 0.7,
-          }}
-        />
-        <Typography variant="h4" marked="center" component="h2" sx={{ mb: 14 }}>
-          How it works
-        </Typography>
-        <div>
-          <Grid container spacing={5}>
-            <Grid item xs={12} md={4}>
-              <Box sx={item}>
-                <Box sx={number}>1.</Box>
-                <Box
-                  component={Image}
-                  src={productHowItWorks1}
-                  alt="suitcase"
-                  sx={image}
-                />
-                <Typography variant="h5" align="center">
-                  Appointment every Wednesday 9am.
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Box sx={item}>
-                <Box sx={number}>2.</Box>
-                <Box
-                  component={Image}
-                  src={productHowItWorks2}
-                  alt="graph"
-                  sx={image}
-                />
-                <Typography variant="h5" align="center">
-                  First come, first served. Our offers are in limited quantities, so
-                  be quick.
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Box sx={item}>
-                <Box sx={number}>3.</Box>
-                <Box
-                  component={Image}
-                  src={productHowItWorks3}
-                  alt="clock"
-                  sx={image}
-                />
-                <Typography variant="h5" align="center">
-                  {'New offers every week. New experiences, new surprises. '}
-                  {'Your Sundays will no longer be alike.'}
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
-        </div>
-        <Button
-          color="secondary"
-          size="large"
-          variant="contained"
-          component="a"
-          href="/premium-themes/onepirate/sign-up/"
-          sx={{ mt: 8 }}
+            component="section"
+            sx={{
+                display: "flex",
+                bgcolor: "secondary.light",
+                overflow: "hidden",
+            }}
         >
-          Get started
-        </Button>
-      </Container>
-    </Box>
-  );
+            <Container
+                sx={{
+                    mt: 10,
+                    mb: 15,
+                    position: "relative",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Box
+                    component={Image}
+                    src={productCurvyLines}
+                    alt="curvy lines"
+                    sx={{
+                        pointerEvents: "none",
+                        position: "absolute",
+                        top: -180,
+                        opacity: 0.7,
+                    }}
+                />
+                <Typography
+                    variant="h4"
+                    marked="center"
+                    component="h2"
+                    sx={{ mb: 7 }}
+                >
+                    How it works
+                </Typography>
+                <div>
+                    <Grid container spacing={5}>
+                        <Grid item xs={12} md={4}>
+                            <Box sx={item}>
+                                <Box sx={number}>1. </Box>
+                                <Box
+                                    component={Image}
+                                    src={productHowItWorks1}
+                                    alt="suitcase"
+                                    sx={image}
+                                />
+                                <Typography variant="h5" align="center">
+                                    Add your activties to MENTOR.
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Box sx={item}>
+                                <Box sx={number}>2.</Box>
+                                <Box
+                                    component={Image}
+                                    src={magic}
+                                    alt="graph"
+                                    sx={image}
+                                />
+                                <Typography variant="h5" align="center">
+                                    Let MENTOR schedule them according to all of
+                                    your preferences.
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Box sx={item}>
+                                <Box sx={number}>3.</Box>
+                                <Box
+                                    component={Image}
+                                    src={schedule}
+                                    alt="clock"
+                                    sx={image}
+                                />
+                                <Typography variant="h5" align="center">
+                                    Build better schedule that suitable to you
+                                    and save time!
+                                </Typography>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </div>
+                <Button
+                    color="secondary"
+                    size="large"
+                    variant="contained"
+                    component="a"
+                    href="/in/sign-up/"
+                    sx={{ mt: 8 }}
+                >
+                    Get started
+                </Button>
+            </Container>
+        </Box>
+    );
 }
 
 export default ProductHowItWorks;
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,34 @@
-import * as React from 'react';
-import ProductCategories from '/modules/views/ProductCategories';
-import ProductSmokingHero from '/modules/views/ProductSmokingHero';
-import AppFooter from '/modules/views/AppFooter';
-import ProductHero from '/modules/views/ProductHero';
-import ProductValues from '/modules/views/ProductValues';
-import ProductHowItWorks from '/modules/views/ProductHowItWorks';
-import ProductCTA from '/modules/views/ProductCTA';
-import AppAppBar from '/modules/views/AppAppBar';
-import withRoot from '/modules/withRoot';
-import { useSelector } from 'react-redux';
+import * as React from "react";
+import ProductCategories from "/modules/views/ProductCategories";
+import ProductSmokingHero from "/modules/views/ProductSmokingHero";
+import AppFooter from "/modules/views/AppFooter";
+import ProductHero from "/modules/views/ProductHero";
+import ProductValues from "/modules/views/ProductValues";
+import ProductHowItWorks from "/modules/views/ProductHowItWorks";
+import ProductCTA from "/modules/views/ProductCTA";
+import AppAppBar from "/modules/views/AppAppBar";
+import withRoot from "/modules/withRoot";
+import { useSelector } from "react-redux";
 import { selectAuthState, setAuthState } from "/modules/model/auth";
 // import appointments from "/public/demo_data/month_appointments.js";
 // import Demo from '../modules/components/demo';
 
 function Index() {
-  const authState = useSelector(selectAuthState);
-  console.log("Auth State INDEX", authState);
-  // const [data, setData] = React.useState(appointments);
-  return (
-    <React.Fragment>
-        <AppAppBar />
-        <ProductHero />
-        <ProductValues />
-        <ProductCategories />
-        <ProductHowItWorks />
-        <ProductCTA />
-        <ProductSmokingHero />
-        <AppFooter />
-    </React.Fragment>
-  );
-  // return (
-  //   <Demo data={data} setData={setData} />
-  // );
+    const authState = useSelector(selectAuthState);
+    console.log("Auth State INDEX", authState);
+    // const [data, setData] = React.useState(appointments);
+    return (
+        <React.Fragment>
+            <AppAppBar />
+            <ProductHero />
+            <ProductHowItWorks />
+            <ProductCategories />
+            <AppFooter />
+        </React.Fragment>
+    );
+    // return (
+    //   <Demo data={data} setData={setData} />
+    // );
 }
 
 export default withRoot(Index);
